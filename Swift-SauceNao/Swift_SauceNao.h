3f6a5c5bd451514f9b288bc3c387a7c2d8f9ff98
Vinrobot <vinrobot97@gmail.com>
2019-02-14 23:01:02 +0100

    Add Xcode project

---
+++ Swift-SauceNao/Swift_SauceNao.h
@@ -0,0 +1,19 @@
+//
+//  Swift_SauceNao.h
+//  Swift-SauceNao
+//
+//  Created by Jaquet Vincent on 14.02.19.
+//  Copyright © 2019 Vinrobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swift_SauceNao.
+FOUNDATION_EXPORT double Swift_SauceNaoVersionNumber;
+
+//! Project version string for Swift_SauceNao.
+FOUNDATION_EXPORT const unsigned char Swift_SauceNaoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swift_SauceNao/PublicHeader.h>
+
+
---
+++ Swift-SauceNaoTests/Swift_SauceNaoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_SauceNaoTests.swift
+//  Swift-SauceNaoTests
+//
+//  Created by Jaquet Vincent on 14.02.19.
+//  Copyright © 2019 Vinrobot. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_SauceNao
+
+class Swift_SauceNaoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
